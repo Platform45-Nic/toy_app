32f6637bd14c1bae94332aef72067ea786474569
Platform45-Nic <nicolo@platform45.com>
2019-07-09 17:51:37 +0200

    Adding Chapter 7 changes: Finish user signup

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   resources :users
 
   get  '/signup',  to: 'users#new'
+  post '/signup',  to: 'users#create'
 
   get  '/home',    to: 'static_pages#home'
   get  '/help',    to: 'static_pages#help'
